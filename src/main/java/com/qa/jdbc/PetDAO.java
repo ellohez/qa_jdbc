6d268e1340ad811796255f53b9e1b0afca315d94
Admin <Admin@192.168.1.100>
2023-03-22 15:05:44 +0000

    Connection moved to private class variable

--- src/main/java/com/qa/jdbc/PetDAO.java
+++ src/main/java/com/qa/jdbc/PetDAO.java
@@ -11,6 +11,8 @@ public class PetDAO {
 	private String url = "";
 	private String user = "";
 	private String pwd = "";
+	private Connection conn;
+
 //	private Connection conn = new Connection();
 
 	public PetDAO(String url, String user, String pwd) {
@@ -18,16 +20,29 @@ public class PetDAO {
 		this.url = url;
 		this.user = user;
 		this.pwd = pwd;
+
+		try {
+			this.conn = DriverManager.getConnection(this.url, this.user, this.pwd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+
+//	public void create(String name, int age, String colour, String breed) {
+//		try (PreparedStatement stmt = this.conn.prepareStatement("INSERT INTO ")) {
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	/**
 	 * @return
 	 */
 	public void read() {
 
-		try (Connection conn = DriverManager.getConnection(this.url, this.user, this.pwd);) {
-			Statement stmt = conn.createStatement();
-			ResultSet results = stmt.executeQuery("SELECT * FROM pet");
+		try (Statement stmt = conn.createStatement();) {
+			ResultSet results = stmt.executeQuery("SELECT * FROM pets");
 			while (results.next()) {
 				System.out.printf("%d, %s, %d, %s %s %n", results.getInt(1), results.getString("name"),
 						results.getInt(3), results.getString("colour"), results.getString("breed"));
@@ -37,4 +52,5 @@ public class PetDAO {
 			e.printStackTrace();
 		}
 	}
+
 }
--- src/main/java/com/qa/jdbc/Runner.java
+++ src/main/java/com/qa/jdbc/Runner.java
@@ -5,8 +5,12 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		PetDAO petDB = new PetDAO("jdbc:mysql://localhost:3306/pets", "root", "pass");
-		petDB.read();
-	}
+		try {
+			PetDAO petDB = new PetDAO("jdbc:mysql://localhost:3306/pet", "root", "pass");
+			petDB.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
 }
